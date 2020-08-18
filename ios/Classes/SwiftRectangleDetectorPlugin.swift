ec022a4b59a56f75b2bb1a5c6af67c2d0d48f5e8
I Wayan Surya Adi Yasa <surya@oddbit.id>
2020-08-18 11:42:05 +0800

    init FlutterResult and implement VNDocumentCameraViewControllerDelegate

--- ios/Classes/SwiftRectangleDetectorPlugin.swift
+++ ios/Classes/SwiftRectangleDetectorPlugin.swift
@@ -2,7 +2,8 @@ import Flutter
 import UIKit
 import VisionKit
 
-public class SwiftRectangleDetectorPlugin: NSObject, FlutterPlugin {
+public class SwiftRectangleDetectorPlugin: NSObject, FlutterPlugin, VNDocumentCameraViewControllerDelegate {
+  var result: FlutterResult? = nil
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "rectangle_detector", binaryMessenger: registrar.messenger())
     let instance = SwiftRectangleDetectorPlugin()
@@ -11,6 +12,19 @@ public class SwiftRectangleDetectorPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
+  }
+
+  public func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+        UIWindow.key?.rootViewController?.dismiss(animated: true, completion: nil)
+  }
+
+  public func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+      print("Document Scanner did fail with Error \(error.localizedDescription)")
+      UIWindow.key?.rootViewController?.dismiss(animated: true, completion: nil)
+  }
+
+  public func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+      UIWindow.key?.rootViewController?.dismiss(animated: true, completion: nil)
   }
 }
 
