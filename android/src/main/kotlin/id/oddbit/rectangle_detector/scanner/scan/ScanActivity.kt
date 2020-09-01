79874f00c9fc2cfdfe3914b83640d2884c5828b8
I Wayan Surya Adi Yasa <surya@oddbit.id>
2020-09-01 13:06:58 +0800

    edit scan activity

--- android/src/main/kotlin/id/oddbit/rectangle_detector/scanner/scan/ScanActivity.kt
+++ android/src/main/kotlin/id/oddbit/rectangle_detector/scanner/scan/ScanActivity.kt
@@ -4,28 +4,26 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
-import android.graphics.Point
 import android.graphics.Rect
 import android.graphics.YuvImage
 import android.hardware.Camera
-import android.media.MediaActionSound
 import android.os.Bundle
 import android.util.Log
 import android.view.SurfaceHolder
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import id.oddbit.rectangle_detector.scanner.SourceManager
 import id.oddbit.rectangle_detector.scanner.base.BaseActivity
+import id.oddbit.rectangle_detector.scanner.camera_preview.CameraPreviewViewModel
+import id.oddbit.rectangle_detector.scanner.crop.CropActivity
 import id.oddbit.rectangle_detector.scanner.processor.Corners
 import id.oddbit.rectangle_detector.scanner.processor.processPicture
-import id.oddbit.rectangle_detector.scanner.crop.CropActivity
-import oddbit.rectangle_detector.rectangle_detector.R
 import io.reactivex.Observable
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_scan.*
+import oddbit.rectangle_detector.rectangle_detector.R
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
 import org.opencv.core.Core
@@ -39,12 +37,13 @@ import java.io.IOException
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-class ScanActivity : BaseActivity(), SurfaceHolder.Callback, Camera.PictureCallback, Camera.PreviewCallback {
+class ScanActivity : BaseActivity() , Camera.PreviewCallback, Camera.PictureCallback{
     private var busy: Boolean = false
     private var mCamera: Camera? = null
     private lateinit var mSurfaceHolder: SurfaceHolder
     private lateinit var executor: ExecutorService
     private lateinit var proxySchedule: Scheduler
+    private lateinit var cameraPreviewViewModel: CameraPreviewViewModel
 
     override fun provideContentViewId(): Int = R.layout.activity_scan
 
@@ -52,11 +51,9 @@ class ScanActivity : BaseActivity(), SurfaceHolder.Callback, Camera.PictureCallb
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        mSurfaceHolder = surface.holder
-        mSurfaceHolder.addCallback(this)
         executor = Executors.newSingleThreadExecutor()
         proxySchedule = Schedulers.from(executor)
+
     }
 
     override fun prepare() {
@@ -71,138 +68,29 @@ class ScanActivity : BaseActivity(), SurfaceHolder.Callback, Camera.PictureCallb
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), Companion.REQUEST_CAMERA_PERMISSION)
         } else if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), Companion.REQUEST_CAMERA_PERMISSION)
-        }
-
-        shut.setOnClickListener {
-            takePicture()
-        }
-    }
-
-    private fun start() {
-        mCamera?.startPreview() ?: Log.i(TAG, "camera null")
-    }
-
-    private fun stop() {
-        mCamera?.stopPreview() ?: Log.i(TAG, "camera null")
-    }
-
-    private fun takePicture() {
-        busy = true
-        Log.i(TAG, "try to focus")
-
-        mCamera?.autoFocus { b, _ ->
-            Log.i(TAG, "focus result: $b")
-
-            mCamera?.takePicture(null, null, this)
-            MediaActionSound().play(MediaActionSound.SHUTTER_CLICK)
-        }
-    }
-
-    private fun updateCamera() {
-        if (null == mCamera) {
-            return
-        }
-        mCamera?.stopPreview()
-        try {
-            mCamera?.setPreviewDisplay(mSurfaceHolder)
-        } catch (e: IOException) {
-            e.printStackTrace()
-            return
-        }
-        mCamera?.setPreviewCallback(this)
-        mCamera?.startPreview()
-    }
-
-    private fun initCamera() {
-        try {
-            mCamera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK)
-        } catch (e: RuntimeException) {
-            e.stackTrace
-            Toast.makeText(this, "cannot open camera, please grant camera", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        val param = mCamera?.parameters
-        val size = firstSupportedPreviewSize()
-        param?.setPreviewSize(size?.width ?: 1920, size?.height ?: 1080)
-
-        val point = Point()
-        val displayWidth = minOf(point.x, point.y)
-        val displayHeight = maxOf(point.x, point.y)
-        val displayRatio = displayWidth.div(displayHeight.toFloat())
-        val previewRatio = size?.height?.toFloat()?.div(size.width.toFloat()) ?: displayRatio
-        val width = mCamera?.parameters?.previewSize?.width ?: 0
-
-        val surfaceParams = this.surface.layoutParams
-        surfaceParams.width = width
-        ///default height is match_parent, recalculate when orientation is horizontal or screen display is tablet
-        if (displayRatio > previewRatio) {
-            surfaceParams.height = (displayHeight / displayRatio * previewRatio).toInt()
-
-        }
-        surface.layoutParams = surfaceParams
-        paper_rect.layoutParams = surfaceParams
-
-        val supportPicSize = mCamera?.parameters?.supportedPictureSizes
-        supportPicSize?.sortByDescending { it.width.times(it.height) }
-
-        val firstPictureSize = supportPicSize?.first()
-        firstPictureSize?.let {
-            param?.setPictureSize(it.width, it.height)
-        }
-
-        val pm = packageManager
-
-        if (pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_AUTOFOCUS)) {
-            param?.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE
-            Log.d(TAG, "AutoFocus enabled")
         } else {
-            Log.d(TAG, "AutoFocus disabled")
+            mCamera = getCameraInstance()
+            mCamera!!.setPreviewCallback(this)
+            cameraPreviewViewModel = CameraPreviewViewModel(this,  fl_camera_preview, shut, paper_rect, mCamera, this)
+            cameraPreviewViewModel.startCameraPreview()
+            cameraPreviewViewModel.captureImage()
         }
 
-        param?.flashMode = Camera.Parameters.FLASH_MODE_AUTO
-
-        mCamera?.parameters = param
-        mCamera?.setDisplayOrientation(90)
     }
 
-    override fun onStart() {
-        super.onStart()
-        start()
+    companion object {
+        private const val REQUEST_CAMERA_PERMISSION = 0
+        private const val REQUEST_CROPPED_PATH = 1
+        private const val EXIT_TIME_THRESHOLD = 2000
     }
 
-    override fun onStop() {
-        super.onStop()
-        stop()
+    private fun getCameraInstance(): Camera? {
+        if (mCamera == null) // mCamera = Camera.open(useBackCamera ? 0 : 1);
+            mCamera = Camera.open(0)
+        return mCamera
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == Companion.REQUEST_CAMERA_PERMISSION
-                && (grantResults[permissions.indexOf(android.Manifest.permission.CAMERA)] == PackageManager.PERMISSION_GRANTED)) {
-            showMessage(R.string.camera_grant)
-            initCamera()
-            updateCamera()
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
 
-    override fun surfaceCreated(p0: SurfaceHolder?) {
-        initCamera()
-    }
-
-    override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
-        Log.i(TAG, "surface changed")
-        updateCamera()
-    }
-
-    override fun surfaceDestroyed(p0: SurfaceHolder?) {
-        synchronized(this) {
-            mCamera?.stopPreview()
-            mCamera?.setPreviewCallback(null)
-            mCamera?.release()
-            mCamera = null
-        }
-    }
 
     override fun onPictureTaken(p0: ByteArray?, p1: Camera?) {
         Log.i(TAG, "on picture taken")
@@ -288,21 +176,14 @@ class ScanActivity : BaseActivity(), SurfaceHolder.Callback, Camera.PictureCallb
                 }
     }
 
-    private fun firstSupportedPreviewSize(): Camera.Size? = mCamera?.parameters?.supportedPreviewSizes?.maxBy { it.width }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_CROPPED_PATH && resultCode == Activity.RESULT_OK) {
             val intent = Intent()
-
             intent.putExtra("CroppedPath", data?.getStringExtra("CroppedPath") ?: "")
             setResult(Activity.RESULT_OK, intent)
             finish()
         }
     }
 
-    companion object {
-        private const val REQUEST_CAMERA_PERMISSION = 0
-        private const val REQUEST_CROPPED_PATH = 1
-        private const val EXIT_TIME_THRESHOLD = 2000
-    }
+
 }
